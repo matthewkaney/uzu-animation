59450d2bd283686162f9294ae5fa6abd38be3422
Matthew Kaney <matthew.s.kaney@gmail.com>
2025-07-21 14:42:56 -0400

    Start adding Strudel hooks

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,18 @@ const renderer = new WebGLRenderer({ context });
 const scene = new Scene();
 const camera = new Camera();
 
+register("viz", (pat: any) =>
+  pat.onTrigger((hap: any) => {
+    console.log(hap);
+  })
+);
+
+let { image, img } = registerControl("image", "img");
+
+// @ts-ignore
+window.image = image;
+// @ts-ignore
+window.img = img;
+
 renderer.setClearColor(0xff00ff);
 renderer.render(scene, camera);
--- src/strudel.d.ts
+++ src/strudel.d.ts
@@ -6,3 +6,7 @@ declare function getDrawContext(
   id?: string,
   options?: GetDrawContextOptions
 ): WebGL2RenderingContext;
+
+declare function register(...params: any): any;
+
+declare function registerControl(name: string, ...aliases: string[]): any;
